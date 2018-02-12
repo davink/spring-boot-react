629de2346a8250cb70d8ee1fdf917e49a04ae0b8
Davin Kelly <davinkelly@gmail.com>
2018-02-12 21:33:06 +0000

    Working

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import request from 'request'
+
 
 class App extends Component {
+
+//var request = require('request');
+//request('http://www.google.com', function (error, response, body) {
+//  console.log('error:', error); // Print the error if one occurred
+//  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//  console.log('body:', body); // Print the HTML for the Google homepage.
+//});
+
+componentDidMount() {
+    request('http://localhost:8080/test', function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +29,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get KDKDKD, edit <code>src/App.js</code>
+          To get KDKDKD, edit <code>src/App.js</code>jj
         </p>
       </div>
     );
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,9 +1,12 @@
 package hello;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
@@ -14,4 +17,8 @@ public class GreetingController {
         return "greeting";
     }
 
+    @RequestMapping("/test")
+    public ResponseEntity greeting() {
+        return ResponseEntity.accepted().body("Hello World");
+    }
 }
